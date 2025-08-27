31d730eea230ef1c2b14649fa62a57bac19e75f8
outy12 <147165196+outy12@users.noreply.github.com>
2025-08-27 09:59:02 +0800

    Update main.c
    class

--- compiler_c/main.c
+++ compiler_c/main.c
@@ -4,7 +4,220 @@
 #include "lexer.h"
 #include "parser.h"
 #include "codegen.h"
-#include "util.h"
+// 解析类声明
+ClassDecl* parse_class_decl(Parser* parser) {
+    consume_token(parser, TOKEN_KEYWORD, "class");
+    
+    Token name_token = consume_token(parser, TOKEN_IDENTIFIER, NULL);
+    char* class_name = strdup(name_token.value);
+    
+    char* base_class = NULL;
+    if (match_token(parser, TOKEN_KEYWORD, "extends")) {
+        consume_token(parser, TOKEN_KEYWORD, "extends");
+        Token base_token = consume_token(parser, TOKEN_IDENTIFIER, NULL);
+        base_class = strdup(base_token.value);
+    }
+    
+    consume_token(parser, TOKEN_PUNCTUATION, "{");
+    
+    Node** members = NULL;
+    int member_count = 0;
+    
+    while (!match_token(parser, TOKEN_PUNCTUATION, "}") && 
+           !match_token(parser, TOKEN_EOF, NULL)) {
+        // 解析字段、构造函数、方法等
+        if (match_token(parser, TOKEN_KEYWORD, "var")) {
+            FieldDecl* field = parse_field_decl(parser);
+            members = realloc(members, sizeof(Node*) * (member_count + 1));
+            members[member_count++] = create_field_decl_node(field);
+        } else if (match_token(parser, TOKEN_KEYWORD, "fn")) {
+            Token fn_name = peek_token(parser);
+            if (strcmp(fn_name.value, "this") == 0) {
+                ConstructorDecl* ctor = parse_constructor_decl(parser);
+                members = realloc(members, sizeof(Node*) * (member_count + 1));
+                members[member_count++] = create_constructor_node(ctor);
+            } else if (strcmp(fn_name.value, "~this") == 0) {
+                // 解析析构函数
+                MethodDecl* dtor = parse_destructor_decl(parser);
+                members = realloc(members, sizeof(Node*) * (member_count + 1));
+                members[member_count++] = create_method_node(dtor);
+            } else {
+                MethodDecl* method = parse_method_decl(parser);
+                members = realloc(members, sizeof(Node*) * (member_count + 1));
+                members[member_count++] = create_method_node(method);
+            }
+        } else if (match_token(parser, TOKEN_KEYWORD, "static")) {
+            // 解析静态成员
+            consume_token(parser, TOKEN_KEYWORD, "static");
+            if (match_token(parser, TOKEN_KEYWORD, "var")) {
+                FieldDecl* field = parse_field_decl(parser);
+                field->is_static = 1;
+                members = realloc(members, sizeof(Node*) * (member_count + 1));
+                members[member_count++] = create_field_decl_node(field);
+            } else if (match_token(parser, TOKEN_KEYWORD, "fn")) {
+                MethodDecl* method = parse_method_decl(parser);
+                method->is_static = 1;
+                members = realloc(members, sizeof(Node*) * (member_count + 1));
+                members[member_count++] = create_method_node(method);
+            }
+        } else {
+            // 错误处理
+            parser_error(parser, "Unexpected token in class declaration");
+            break;
+        }
+    }
+    
+    consume_token(parser, TOKEN_PUNCTUATION, "}");
+    
+    ClassDecl* class_decl = malloc(sizeof(ClassDecl));
+    class_decl->name = class_name;
+    class_decl->base_class = base_class;
+    class_decl->members = members;
+    class_decl->member_count = member_count;
+    
+    return class_decl;
+}
+
+// 解析构造函数
+ConstructorDecl* parse_constructor_decl(Parser* parser) {
+    consume_token(parser, TOKEN_KEYWORD, "fn");
+    consume_token(parser, TOKEN_KEYWORD, "this");
+    
+    consume_token(parser, TOKEN_PUNCTUATION, "(");
+    Node** params = parse_param_list(parser);
+    consume_token(parser, TOKEN_PUNCTUATION, ")");
+    
+    Node* base_call = NULL;
+    if (match_token(parser, TOKEN_PUNCTUATION, ":")) {
+        consume_token(parser, TOKEN_PUNCTUATION, ":");
+        consume_token(parser, TOKEN_KEYWORD, "base");
+        consume_token(parser, TOKEN_PUNCTUATION, "(");
+        Node** base_args = parse_arg_list(parser);
+        consume_token(parser, TOKEN_PUNCTUATION, ")");
+        
+        base_call = create_super_call_node(base_args, count_args(base_args));
+    }
+    
+    Node* body = parse_block(parser);
+    
+    ConstructorDecl* ctor = malloc(sizeof(ConstructorDecl));
+    ctor->params = params;
+    ctor->param_count = count_params(params);
+    ctor->body = body;
+    ctor->base_call = base_call;
+    
+    return ctor;
+}
+
+// 解析方法声明
+MethodDecl* parse_method_decl(Parser* parser) {
+    int is_static = 0;
+    if (match_token(parser, TOKEN_KEYWORD, "static")) {
+        consume_token(parser, TOKEN_KEYWORD, "static");
+        is_static = 1;
+    }
+    
+    consume_token(parser, TOKEN_KEYWORD, "fn");
+    
+    Token name_token = consume_token(parser, TOKEN_IDENTIFIER, NULL);
+    char* method_name = strdup(name_token.value);
+    
+    consume_token(parser, TOKEN_PUNCTUATION, "(");
+    Node** params = parse_param_list(parser);
+    consume_token(parser, TOKEN_PUNCTUATION, ")");
+    
+    char* return_type = "void";
+    if (match_token(parser, TOKEN_PUNCTUATION, ":")) {
+        consume_token(parser, TOKEN_PUNCTUATION, ":");
+        Token type_token = consume_token(parser, TOKEN_IDENTIFIER, NULL);
+        return_type = strdup(type_token.value);
+    }
+    
+    Node* body = parse_block(parser);
+    
+    MethodDecl* method = malloc(sizeof(MethodDecl));
+    method->name = method_name;
+    method->params = params;
+    method->param_count = count_params(params);
+    method->return_type = return_type;
+    method->body = body;
+    method->is_static = is_static;
+    
+    return method;
+}
+
+// 解析字段声明
+FieldDecl* parse_field_decl(Parser* parser) {
+    consume_token(parser, TOKEN_KEYWORD, "var");
+    
+    Token name_token = consume_token(parser, TOKEN_IDENTIFIER, NULL);
+    char* field_name = strdup(name_token.value);
+    
+    char* field_type = NULL;
+    Node* initial_value = NULL;
+    
+    if (match_token(parser, TOKEN_PUNCTUATION, ":")) {
+        consume_token(parser, TOKEN_PUNCTUATION, ":");
+        Token type_token = consume_token(parser, TOKEN_IDENTIFIER, NULL);
+        field_type = strdup(type_token.value);
+    }
+    
+    if (match_token(parser, TOKEN_OPERATOR, "=")) {
+        consume_token(parser, TOKEN_OPERATOR, "=");
+        initial_value = parse_expr(parser);
+    }
+    
+    consume_token(parser, TOKEN_PUNCTUATION, ";");
+    
+    FieldDecl* field = malloc(sizeof(FieldDecl));
+    field->name = field_name;
+    field->type = field_type;
+    field->initial_value = initial_value;
+    field->is_static = 0;
+    
+    return field;
+}
+
+// 解析 new 表达式
+NewExpr* parse_new_expr(Parser* parser) {
+    consume_token(parser, TOKEN_KEYWORD, "new");
+    
+    Token class_token = consume_token(parser, TOKEN_IDENTIFIER, NULL);
+    char* class_name = strdup(class_token.value);
+    
+    consume_token(parser, TOKEN_PUNCTUATION, "(");
+    Node** args = parse_arg_list(parser);
+    consume_token(parser, TOKEN_PUNCTUATION, ")");
+    
+    NewExpr* new_expr = malloc(sizeof(NewExpr));
+    new_expr->class_name = class_name;
+    new_expr->args = args;
+    new_expr->arg_count = count_args(args);
+    
+    return new_expr;
+}
+
+// 解析成员访问表达式
+MemberAccess* parse_member_access(Parser* parser, Node* object) {
+    consume_token(parser, TOKEN_OPERATOR, ".");
+    
+    Token member_token = consume_token(parser, TOKEN_IDENTIFIER, NULL);
+    char* member_name = strdup(member_token.value);
+    
+    MemberAccess* access = malloc(sizeof(MemberAccess));
+    access->object = object;
+    access->member_name = member_name;
+    
+    return access;
+}
+
+// 解析 this 表达式
+ThisExpr* parse_this_expr(Parser* parser) {
+    consume_token(parser, TOKEN_KEYWORD, "this");
+    
+    ThisExpr* this_expr = malloc(sizeof(ThisExpr));
+    return this_expr;
+}
 
 int compile_file(const char* filename, const char* output_filename) {
     // 读取源文件
